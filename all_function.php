08b225c41004156a9546d07fbb90ac35fcc6d3de
chin007x <chinnawatq@gmail.com>
2021-12-21 11:25:24 +0700

    v.2

---
+++ all_function.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Prompt:ital,wght@0,100;0,200;0,300;1,100&display=swap" rel="stylesheet">
+<style>
+    body{
+        font-family: 'Prompt', sans-serif;
+    }
+</style>
+</head>
+<body>
+    <?php
+        function showmesssage()
+            {
+                echo"<center>";
+                echo"<h2>ยินดีต้อนรับสู่เว็บไไซต์ </h2>";
+                echo"<h3>เพื่อประโยชน์ของสมาชิก</h3>";
+                echo"<h4>โปรดติดตามข่าวสารจากเว็บไซต์ ผ่านสือดังนี้</h4>";
+                echo"<h5>e-mail ,sms และ เว็บบอร์</h5>";
+                echo"<hr>";
+                echo"</center>";
+            
+            }
+
+
+
+
+
+
+
+
+
+        
+        ?>
+
+</body>
+</html>
